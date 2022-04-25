a0927098295497b8dde61f15d897f23a98d03e5f
nt4rever <levantanald@gmail.com>
2022-04-25 20:28:39 +0700

    add some test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import shutil
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
+import uvicorn
 import point
+import filter
 
 app = FastAPI()
 
@@ -38,6 +40,7 @@ async def root(file: UploadFile = File(...), c: float = 2):
     res = "exports/"+file.filename
     return res
 
+
 @app.post("/point/hist", response_class=FileResponse)
 async def root(file: UploadFile = File(...)):
     with open(f'uploads/{file.filename}', 'wb') as buffer:
@@ -47,3 +50,31 @@ async def root(file: UploadFile = File(...)):
     point.export_image(image, "exports/"+file.filename)
     res = "exports/"+file.filename
     return res
+
+
+@app.post("/filter/gaussian_blur", response_class=FileResponse)
+async def root(file: UploadFile = File(...), x: int = 5):
+    with open(f'uploads/{file.filename}', 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    path = "./uploads/"+file.filename
+    image = filter.gaussian_blur(path, x)
+    point.export_image(image, "exports/"+file.filename)
+    res = "exports/"+file.filename
+    return res
+
+@app.post("/filter/laplacian", response_class=FileResponse)
+async def root(file: UploadFile = File(...), k: int = 3):
+    with open(f'uploads/{file.filename}', 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    path = "./uploads/"+file.filename
+    image = filter.laplacian(path, k)
+    point.export_image(image, "exports/"+file.filename)
+    res = "exports/"+file.filename
+    return res
+
+if __name__ == '__main__':
+    uvicorn.run("app.main:app",
+                host="0.0.0.0",
+                port=8000,
+                reload=True,
+                )
